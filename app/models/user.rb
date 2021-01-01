cb718e86c0f7b8b4b1fd0db908eea1e6bf448d9e
enaena <e_ngu_no1@yahoo.co.jp>
2021-01-02 02:38:18 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :image
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角の英字と数字の両方を含めて設定してください' 
+  
+  validates :nickname, presence: true
+  validates :image, presence: true
+
+  with_options numericality: { other_than: 1, message: 'を選択してください'  } do
+    validates :age_id
+    validates :sex_id
+    validates :level_id
+  end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :age
     belongs_to_active_hash :sex
     belongs_to_active_hash :level
+
+  has_one_attached :image
     
 end
