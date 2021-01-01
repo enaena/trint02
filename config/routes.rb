deee328a2933aa8ab1eb8c9d6b08166735f8882d
enaena <e_ngu_no1@yahoo.co.jp>
2021-01-02 06:42:31 +0900

    イベント詳細画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'top#index'
-  resources :events, only: [:index, :new, :create, :show]
+  resources :users, only: [:edit, :update, :show]
+  resources :events, only: [:index, :new, :create, :show] do
+    resources :event_users, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20210101203431_create_event_users.rb
+++ db/migrate/20210101203431_create_event_users.rb
@@ -1,7 +1,9 @@
 class CreateEventUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :event_users do |t|
-
+      t.integer :event_id
+      t.integer :user_id
+      t.text    :text
       t.timestamps
     end
   end
