5c303987be0dbccbe1f8949665fe31bcb06d0466
enaena <e_ngu_no1@yahoo.co.jp>
2021-01-02 02:22:44 +0900

    ログイン・新規登録画面を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :age
     belongs_to_active_hash :sex
